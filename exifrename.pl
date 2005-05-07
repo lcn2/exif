20514867a2a1a22863a9f47ab6939c9960cbcc01
Landon Curt Noll <github-mail@asthe.com>
2005-05-07 01:05:09 +0000

    fixed some syntax errors, moved doc to wanted function --chongo

--- exifrename.pl
+++ exifrename.pl
@@ -2,9 +2,9 @@
 #
 # exifrename - copy files based on EXIF or file time data
 #
-# @(#) $Revision: 1.8 $
-# @(#) $Id: exif.pl,v 1.8 2005/05/04 16:15:23 chongo Exp chongo $
-# @(#) $Source: /usr/local/src/cmd/exif/RCS/exif.pl,v $
+# @(#) $Revision: 1.1 $
+# @(#) $Id: exifrename.pl,v 1.1 2005/05/05 21:30:03 chongo Exp chongo $
+# @(#) $Source: /usr/local/src/cmd/exif/RCS/exifrename.pl,v $
 #
 # Copyright (c) 2005 by Landon Curt Noll.  All Rights Reserved.
 #
@@ -37,12 +37,12 @@ use bytes;
 use vars qw($opt_h $opt_v);
 use Getopt::Long;
 use Image::ExifTool qw(ImageInfo);
-no warnings 'File::Find';
 use File::Find;
+no warnings 'File::Find';
 
 # version - RCS style *and* usable by MakeMaker
 #
-my $VERSION = substr q$Revision: 1.8 $, 10;
+my $VERSION = substr q$Revision: 1.1 $, 10;
 $VERSION =~ s/\s+$//;
 
 # my vars
@@ -53,12 +53,12 @@ my @tag_list = qw( ModifyDate DateTimeOriginal CreateDate );
 my $mintime = 750000000;
 my $srcdir;	# source of image files
 my $destdir;	# where the renamed files will be copied
-my $setdir;	# $destdir/set where set symlinks go
+my $rolldir;	# $destdir/roll under where roll symlinks go
 my $exiftool;	# Image::ExifTool object
 
 # usage and help
 #
-my $usage = "$0 [-h][-v lvl] srcdir destdir"
+my $usage = "$0 [-h][-v lvl] srcdir destdir";
 my $help = qq{\n$usage
 
 	-h	    print this help message
@@ -79,6 +79,7 @@ my %optctl = (
 
 # function prototypes
 #
+sub wanted();
 sub dir_setup();
 sub exif_setup();
 sub exif_date($$);
@@ -88,6 +89,7 @@ sub file_date($);
 # setup
 #
 MAIN: {
+    my %find_opt;		# File::Find directory tree walk options
 
     # setup
     #
@@ -119,7 +121,7 @@ MAIN: {
     }
     $srcdir = $ARGV[0];
     $destdir = $ARGV[1];
-    $setdir = "$destdir/set";
+    $rolldir = "$destdir/roll";
 
     # setup directories
     #
@@ -129,90 +131,100 @@ MAIN: {
     #
     $exiftool = exif_setup();
 
-    # process all files and directories under $srcdir
+    # setup to walk the srcdir
     #
-    # XXX - move these comments to the find wanted function
-    #
-    # We we process files under $srcdir, we copy them to $destdir and
-    # we build up the symlink tree under $setdir.
-    #
-    # Consider the a file under srcdir:
-    #
-    #	/srcdir/DCIM/101EOS1D/LS1F5627.CR2
-    #
-    # Assume that the EXIF timestamp (or file timestamp if if lacks
-    # EXIF timestamp tags) is:
-    #
-    #	2005-05-15
-    #
-    # Then we will create the file:
-    #
-    #	/destdir/200505/20050515-ls1f5627.cr2
-    #
-    # The created file path is:
-    #
-    #	/destdir
-    #	4 digit Year of the timestamp
-    #	Month [01-12] of the timestamp
-    #	/
-    #	4 digit Year of the timestamp
-    #	Month [01-12] of the timestamp
-    #   Day [01-31] of the timestamp
-    #	- (dash)
-    #	basename of srcfile mapped to lower case
-    #
-    # In addition, a symlink is setup under setdir as follows:
-    #
-    #	/destdir/set/set0031/101eos1d/20050515-ls1f5627.cr2 ->
-    #	    ../../20050515-ls1f5627.cr2
-    #
-    # Note that the symlink filename basename is the same as the destination 
-    # filename.  The symlink path is:
-    #
-    #	/destdir
-    #	/set
-    #	/setWXYZ	(WXYZ is a 4 digit number)
-    #	/101eos1d	(the directory path under /srcdir/DCIM lowercased)
-    #	/
-    #	4 digit Year of the timestamp
-    #	Month [01-12] of the timestamp
-    #   Day [01-31] of the timestamp
-    #	- (dash)
-    #	basename of srcfile mapped to lower case
-    #
-    #
-    # The WXYZ is a 4 digit serial number from the file:
-    #
-    #	~/.exifnum
-    #
-    # It if initialized to 0000 if that file does not exist.  The current
-    # value is used to form the setWXYZ path component and then is incremented
-    # by 1.  If incrementing reaches 10000, it is reset to 0000.  Note that
-    # the WXYZ used in the path is the value prior to incrementing.
-    #
-    # NOTE: The EOS 1D Canon Image filesystem, without any images looks like:
-    #
-    #	/DCIM/			# all image directories go under DCIM
-    #	/DCIM/100EOS1D/		# 1st image directory (2nd is 101EOS1D, ...)
-    #	/.Trashes/		# directory where deleted images go?
-    #	/.Trashes/501/		# ??? directory (other 5xx dirs have been seen)
-    #	/.Trashes/._501		# ??? file (other (._5xx files have beens seen)
-    #
-    # Once the camera deletes an image, this file is created:
-    #
-    #	/constate.tof
-    #
-    # In addition, other files such as .DS_Store may be created by OS X.
-    # These .DS_Store files should be ignored by the tool.
-    #
-    # So we need to ignore the following:
-    #
-    #	/.Trashes		# entire directory tree directly under srcdir
-    #	/.comstate.tof		# this file directly under srcdir
-    #	.DS_Store		# this fiile anywhere
-    #
-    # In addition, for path purposes, we do not create DCIM as a path component
-    # when forming files and symlinks in destdir.
+    $find_opt{wanted} = wanted;
+    # XXX - more code here
+}
+
+
+# wanted - File::Find tree walking function called at each non-pruned node
+#
+# This function is a callback from the File::Find directory tree walker.
+# It will walk the $srcdir and copy/rename files as needed.
+#
+# We we process files under $srcdir, we copy them to $destdir and
+# we build up the symlink tree under $rolldir.
+#
+# Consider the a file under srcdir:
+#
+#	/srcdir/DCIM/101EOS1D/LS1F5627.CR2
+#
+# Assume that the EXIF timestamp (or file timestamp if if lacks
+# EXIF timestamp tags) is:
+#
+#	2005-05-15
+#
+# Then we will create the file:
+#
+#	/destdir/200505/20050515-ls1f5627.cr2
+#
+# The created file path is:
+#
+#	/destdir
+#	4 digit Year of the timestamp
+#	Month [01-12] of the timestamp
+#	/
+#	4 digit Year of the timestamp
+#	Month [01-12] of the timestamp
+#   Day [01-31] of the timestamp
+#	- (dash)
+#	basename of srcfile mapped to lower case
+#
+# In addition, a symlink is setup under rolldir as follows:
+#
+#	/destdir/roll/roll-0031/101eos1d/20050515-ls1f5627.cr2 ->
+#	    ../../20050515-ls1f5627.cr2
+#
+# Note that the symlink filename basename is the same as the destination
+# filename.  The symlink path is:
+#
+#	/destdir
+#	/roll
+#	/roll-WXYZ	(WXYZ is a 4 digit number)
+#	/101eos1d	(the directory path under /srcdir/DCIM lowercased)
+#	/
+#	4 digit Year of the timestamp
+#	Month [01-12] of the timestamp
+#   Day [01-31] of the timestamp
+#	- (dash)
+#	basename of srcfile mapped to lower case
+#
+# The WXYZ is a 4 digit serial number from the file:
+#
+#	~/.exifnum
+#
+# It if initialized to 0000 if that file does not exist.  The current
+# value is used to form the roll-WXYZ path component and then is incremented
+# by 1.  If incrementing reaches 10000, it is reset to 0000.  Note that
+# the WXYZ used in the path is the value prior to incrementing.
+#
+# NOTE: The EOS 1D Canon Image filesystem, without any images looks like:
+#
+#	/DCIM/			# all image directories go under DCIM
+#	/DCIM/100EOS1D/		# 1st image directory (2nd is 101EOS1D, ...)
+#	/.Trashes/		# directory where deleted images go?
+#	/.Trashes/501/		# ??? directory (other 5xx dirs have been seen)
+#	/.Trashes/._501		# ??? file (other (._5xx files have beens seen)
+#
+# Once the camera deletes an image, this file is created:
+#
+#	/constate.tof
+#
+# In addition, other files such as .DS_Store may be created by OS X.
+# These .DS_Store files should be ignored by the tool.
+#
+# So we need to ignore the following:
+#
+#	/.Trashes		# entire directory tree directly under srcdir
+#	/.comstate.tof		# this file directly under srcdir
+#	.DS_Store		# this fiile anywhere
+#
+# In addition, for path purposes, we do not create DCIM as a path component
+# when forming files and symlinks in destdir.
+#
+sub wanted()
+{
 }
 
 
@@ -221,7 +233,7 @@ MAIN: {
 # uses these globals:
 #	$srcdir		where images are from
 #	$desdir		where copied and renamed files go
-#	$setdir		$destdir/set where symlinks are formed
+#	$rolldir	$destdir/roll under where symlinks are formed
 #
 # NOTE: Does not return on error.
 #
@@ -258,7 +270,7 @@ sub dir_setup()
     if (-e $destdir && ! -d $destdir) {
 	print STDERR "$0: destdir is a non-directory: $destdir\n";
 	exit(11);
-    } elsif (! -d $destdir && ! mkdir($destdir, 0775) {
+    } elsif (! -d $destdir && ! mkdir($destdir, 0775)) {
 	print STDERR "$0: cannot make destdir: $destdir: $!\n";
 	exit(12);
     } elsif (! -w $destdir) {
@@ -266,16 +278,16 @@ sub dir_setup()
 	exit(13);
     }
 
-    # setup the set symlink dir if needed
+    # setup the roll symlink dir if needed
     #
-    if (-e $setdir && ! -d $setdir) {
-	print STDERR "$0: destdir/set is a non-directory: $setdir\n";
+    if (-e $rolldir && ! -d $rolldir) {
+	print STDERR "$0: destdir/roll is a non-directory: $rolldir\n";
 	exit(14);
-    } elsif (! -d $setdir && ! mkdir($setdir, 0775) {
-	print STDERR "$0: cannot make destdir/set: $setdir: $!\n";
+    } elsif (! -d $rolldir && ! mkdir($rolldir, 0775)) {
+	print STDERR "$0: cannot make destdir/roll: $rolldir: $!\n";
 	exit(15);
-    } elsif (! -w $setdir) {
-	print STDERR "$0: destdir/set is not writable: $setdir\n";
+    } elsif (! -w $rolldir) {
+	print STDERR "$0: destdir/roll is not writable: $rolldir\n";
 	exit(16);
     }
     return;
@@ -289,7 +301,7 @@ sub dir_setup()
 #
 sub exif_setup()
 {
-    my %exitoptions;			# ExifTool options
+    my %exifoptions;			# ExifTool options
     my $exif_tool;			# Image::ExifTool object
 
     # setup ExifTool options
@@ -297,7 +309,7 @@ sub exif_setup()
     $exifoptions{Binary} = 0;		# no timestamp is a binary field
     $exifoptions{PrintConv} = 0;	# no need to waste time converting
     $exifoptions{Unknown} = 0;		# all timestamps are in known fields
-    $exifoptions{DateFormat} = "\%s"	# timestamps as seconds since the Epoch
+    $exifoptions{DateFormat} = "\%s";	# timestamps as seconds since the Epoch
     $exifoptions{Duplicates} = 0;	# use the last timestamp if we have dups
 
     # create a new Image::ExifTool object
